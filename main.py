68d8a6b3e19e9496197a5d8612b301d28ab4c7ab
Kaysar <kaysarmanken25@gmail.com>
2022-02-13 12:56:47 +0600

    added functional Ai

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ pygame.init()
 
 from classes.paddle import Paddle
 from classes.ball import PongBall
+from classes.pongbot import PongBot
 
 # Constants
 SURF_COLOR = (60, 50, 168) 
@@ -16,6 +17,7 @@ MOVE_PIXELS = 7
 
 # Set up display
 screen = pygame.display.set_mode(SCREEN_SIZE)
+
 screenX = screen.get_width()
 screenY = screen.get_height()
 screenArea = screen.get_rect()
@@ -24,21 +26,24 @@ screenArea = screen.get_rect()
 sprites_list = pygame.sprite.Group()
 
 pong_player = Paddle(PADDLE_SIZE, SPRITE_COLOR, SURF_COLOR)
-pong_bot = Paddle(PADDLE_SIZE, SPRITE_COLOR, SURF_COLOR)
+pong_player2 = Paddle(PADDLE_SIZE, SPRITE_COLOR, SURF_COLOR)
 pong_ball = PongBall(BALL_SIZE, SPRITE_COLOR, SURF_COLOR)
 
 pong_player.rect.x = 20
 pong_player.rect.y = screenY - 300 
 
-pong_bot.rect.x = screenX - 20
-pong_bot.rect.y = screenY - 300
+pong_player2.rect.x = screenX - 20
+pong_player2.rect.y = screenY - 300
 
 pong_ball.rect.x = 30 
 pong_ball.rect.y = 205
 
 sprites_list.add(pong_player)
-sprites_list.add(pong_bot)
+sprites_list.add(pong_player2)
 sprites_list.add(pong_ball)
+
+game_assets = [pong_ball, pong_player, pong_player2, MOVE_PIXELS]
+pong_bot = PongBot(game_assets)
 
 # Setup game loop 
 running = True 
@@ -61,7 +66,7 @@ def handle_ball():
         pong_ball.reflectY()
     if pong_ball.rect.y <= 0:
         pong_ball.reflectY()
-    if pygame.sprite.collide_mask(pong_ball, pong_player) or pygame.sprite.collide_mask(pong_ball, pong_bot):
+    if pygame.sprite.collide_mask(pong_ball, pong_player) or pygame.sprite.collide_mask(pong_ball, pong_player2):
         pong_ball.bounce()
 
 while running:
@@ -72,8 +77,10 @@ while running:
     process_input()
     handle_ball()
 
+    pong_bot.update()
+
     pong_player.rect.clamp_ip(screenArea)
-    pong_bot.rect.clamp_ip(screenArea)
+    pong_player2.rect.clamp_ip(screenArea)
 
     sprites_list.update()
     screen.fill(SURF_COLOR)
